0155ccdde4e667a1984447fc3fbe0268a1612211
debugeverything-project <95109898+debugeverything-project@users.noreply.github.com>
2023-07-29 20:54:57 +1000

    This is an updated task

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app2');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>This is an updated task</h1>`;
