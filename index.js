56f1a9a8b0124da8aff393616aecccdab758e69c
debugeverything-project <95109898+debugeverything-project@users.noreply.github.com>
2023-08-30 11:15:13 +1000

    Updating all this

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app2');
-appDiv.innerHTML = `<h1>This is an updated task</h1>`;
+appDiv.innerHTML = `<h1>This is an updated task xxxx</h1>`;
