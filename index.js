ec2765583e1a1df9bab38a94027bf5966072b9e7
debugeverything-project <95109898+debugeverything-project@users.noreply.github.com>
2023-08-30 11:10:19 +1000

    Testing again

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app2');
-appDiv.innerHTML = `<h1>Testing </h1>`;
+appDiv.innerHTML = `<h1>Testing again </h1>`;
